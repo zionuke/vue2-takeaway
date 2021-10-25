1f1439a6c85b460a17c37d93c25e35803894c1ab
dragon-liu <liuzhichao@hust.edu.cn>
2021-10-25 17:17:45 +0800

    切换显示或隐藏密码

--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -37,10 +37,11 @@
                 <input type="tel" maxlength="11" placeholder="手机/邮箱/用户名">
               </section>
               <section class="login_verification">
-                <input type="tel" maxlength="8" placeholder="密码">
-                <div class="switch_button off">
-                  <div class="switch_circle"></div>
-                  <span class="switch_text">...</span>
+                <input type="text" maxlength="8" placeholder="密码" v-if="showPwd" v-model="pwd">
+                <input type="password" maxlength="8" placeholder="密码" v-else v-model="pwd">
+                <div class="switch_button" :class="showPwd?'on':'off'" @click="showPwd=!showPwd">
+                  <div class="switch_circle" :class="{right:showPwd}"></div>
+                  <span class="switch_text">{{showPwd ? 'abc' :'...'}}</span>
                 </div>
               </section>
               <section class="login_message">
@@ -67,7 +68,9 @@ export default {
     return {
       loginMethod: true, // true代表短信登录，false代表密码登录
       phone: '', // 手机号
-      computeTime: 0 // 计时的时间，默认为0，在点击之后动态修改
+      computeTime: 0, // 计时的时间，默认为0，在点击之后动态修改
+      showPwd: false, // 是否显示密码
+      pwd: '' // 密码
     }
   },
   computed: {
@@ -197,6 +200,8 @@ export default {
                 background #fff
                 box-shadow 0 2px 4px 0 rgba(0,0,0,.1)
                 transition transform .3s
+                &.right 
+                  transform translateX(30px)
           .login_hint
             margin-top 12px
             color #999
