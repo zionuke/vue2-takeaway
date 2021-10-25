b7e2640ff83895e2966b72d3ca8913b16114620c
dragon-liu <liuzhichao@hust.edu.cn>
2021-10-25 16:00:51 +0800

    倒计时效果

--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -14,7 +14,13 @@
           <div :class="{on: loginMethod}">
             <section class="login_message">
               <input type="tel" maxlength="11" placeholder="手机号" v-model="phone">
-              <button disabled="disabled" class="get_verification" :class="{right_phone: rightPhone}">获取验证码</button>
+              <button 
+                :disabled="!rightPhone" 
+                class="get_verification" 
+                :class="{right_phone: rightPhone}"
+                @click.prevent="getCode"
+              >{{computeTime ? `已发送(${computeTime}s)` : '获取验证码'}}
+              </button>
             </section>
             <section class="login_verification">
               <input type="tel" maxlength="8" placeholder="验证码">
@@ -60,7 +66,8 @@ export default {
   data () {
     return {
       loginMethod: true, // true代表短信登录，false代表密码登录
-      phone: '' // 手机号
+      phone: '', // 手机号
+      computeTime: 0 // 计时的时间，默认为0，在点击之后动态修改
     }
   },
   computed: {
@@ -69,10 +76,21 @@ export default {
     }
   },
   methods: {
-
-  },
-  components: {
-
+    getCode () {
+      // 如果当前没有计时
+      if(!this.computeTime){
+        // 启动倒计时
+        this.computeTime = 30
+        const intervalId = setInterval(()=>{
+          this.computeTime--
+          if(this.computeTime === 0){
+            // 停止计时
+            clearInterval(intervalId)
+          }
+        }, 1000)
+        // 发送ajax请求(向指定手机号发送验证码短信)
+      }
+    }
   }
 }
 </script>
