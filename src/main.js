5d833eca6511070f6849c594fa54a6dad0ee07a2
dragon-liu <liuzhichao@hust.edu.cn>
2021-10-26 22:13:33 +0800

    使用mint-ui组件库实现退出登陆

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
+import {Button} from 'mint-ui'
 import App from './App'
 import router from './router'
 import store from './store'
+
+// 注册全局组件标签
+Vue.component(Button.name, Button)  // 全局可使用<mt-button>标签
 
 new Vue({
   el: '#app',
--- src/store/actions.js
+++ src/store/actions.js
@@ -5,13 +5,15 @@ import {
   RECEIVE_ADDRESS,
   RECEIVE_CATEGORYS,
   RECEIVE_SHOPS,
-  RECEIVE_USER_INFO
+  RECEIVE_USER_INFO,
+  RESET_USER_INFO
 } from './mutation-types'
 import {
   reqAddress,
   reqCategorys,
   reqShops,
-  reqUserInfo
+  reqUserInfo,
+  reqLogout
 } from '../api'
 
 export default {
@@ -60,6 +62,14 @@ export default {
     if (result.code === 0) {
       const userInfo = result.data
       commit(RECEIVE_USER_INFO, {userInfo})
+    }
+  },
+
+  // 异步登出
+  async logout({commit}) {
+    const result = await reqLogout()
+    if (result.code === 0) {
+      commit(RESET_USER_INFO)
     }
   },
 }
