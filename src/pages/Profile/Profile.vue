06e5b4fcea34bbdaae1f2afc9213a05addc46a02
dragon-liu <liuzhichao@hust.edu.cn>
2021-10-26 19:42:51 +0800

    登陆后更新个人中心界面

--- src/pages/Profile/Profile.vue
+++ src/pages/Profile/Profile.vue
@@ -7,12 +7,12 @@
           <i class="iconfont icon-person"></i>
         </div>
         <div class="user-info">
-          <p class="user-info-top">{{ userInfo._id || '登录/注册'}}</p>
+          <p class="user-info-top" v-if="!userInfo.phone">{{userInfo.name || '登录/注册'}}</p>
           <p>
             <span class="user-icon">
               <i class="iconfont icon-shouji icon-mobile"></i>
             </span>
-            <span class="icon-mobile-number">暂无绑定手机号</span>
+            <span class="icon-mobile-number">{{userInfo.phone || '暂无绑定手机号'}}</span>
           </p>
         </div>
         <span class="arrow">
