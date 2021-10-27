378b0d3b027954852b39be12d124570c312ecd0d
dragon-liu <liuzhichao@hust.edu.cn>
2021-10-27 16:29:08 +0800

    利用 mockjs 提供模拟数据

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import App from './App'
 import router from './router'
 import store from './store'
 
+import './mock/mockServer' // 加载mockServer即可
+
 // 注册全局组件标签
 Vue.component(Button.name, Button)  // 全局可使用<mt-button>标签
 
