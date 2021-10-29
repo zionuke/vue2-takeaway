c01f8bbf7e6a6a7b5e740345c79446aee03fde78
dragon-liu <liuzhichao@hust.edu.cn>
2021-10-30 00:56:54 +0800

    开发 CartControl 组件

--- src/components/CartControl/CartControl.vue
+++ src/components/CartControl/CartControl.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="cartcontrol">
     <transition name="move">
-      <div class="iconfont icon-remove_circle_outline" v-if="food.count"></div>
+      <div class="iconfont icon-remove_circle_outline" v-if="food.count" @click.stop="updateFoodCount(false)"></div>
     </transition>
     <div class="cart-count" v-if="food.count">{{food.count}}</div>
-    <div class="iconfont icon-add_circle"></div>
+    <div class="iconfont icon-add_circle" @click.stop="updateFoodCount(true)"></div>
   </div>
 </template>
 
@@ -15,7 +15,9 @@
     },
 
     methods: {
-
+      updateFoodCount (isAdd) {
+        this.$store.dispatch('updateFoodCount', {isAdd, food: this.food})
+      }
     }
   }
 </script>
--- src/store/actions.js
+++ src/store/actions.js
@@ -10,6 +10,8 @@ import {
   RECEIVE_GOODS,
   RECEIVE_RATINGS,
   RECEIVE_INFO,
+  INCREMENT_FOOD_COUNT,
+  DECREMENT_FOOD_COUNT,
 } from './mutation-types'
 import {
   reqAddress,
@@ -107,6 +109,15 @@ export default {
       commit(RECEIVE_GOODS, {goods})
       // 数据更新了, 通知一下组件
       callback && callback()
+    }
+  },
+
+  // 同步更新food中的count值
+  updateFoodCount({commit}, {isAdd, food}) {
+    if (isAdd) {
+      commit(INCREMENT_FOOD_COUNT, {food})
+    } else {
+      commit(DECREMENT_FOOD_COUNT, {food})
     }
   },
 }
