406c0ff659a57823da17e7a53c150c6aa3354f26
dragon-liu <liuzhichao@hust.edu.cn>
2021-11-01 22:15:51 +0800

    自定义过滤器

--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ import store from './store'
 
 import './mock/mockServer' // 加载mockServer即可
 import loading from './common/imgs/loading.gif'
+import './filters' // 加载过滤器
 
 // 注册全局组件标签
 Vue.component(Button.name, Button)  // 全局可使用<mt-button>标签
