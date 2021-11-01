23dc9770509eb88144831efbc33e97744dc2ec9f
dragon-liu <liuzhichao@hust.edu.cn>
2021-11-01 21:54:50 +0800

    图片懒加载: vue-lazyload

--- src/main.js
+++ src/main.js
@@ -1,13 +1,21 @@
+/*
+入口JS
+ */
 import Vue from 'vue'
-import {Button} from 'mint-ui'
+import { Button } from 'mint-ui'
+import VueLazyload from 'vue-lazyload'
 import App from './App'
 import router from './router'
 import store from './store'
 
 import './mock/mockServer' // 加载mockServer即可
+import loading from './common/imgs/loading.gif'
 
 // 注册全局组件标签
 Vue.component(Button.name, Button)  // 全局可使用<mt-button>标签
+Vue.use(VueLazyload, { // 内部自定义一个指令lazy
+  loading
+})
 
 new Vue({
   el: '#app',
