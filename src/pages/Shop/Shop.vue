e0f17cd891ad6c44c640633abc1aa0c5d73b5308
dragon-liu <liuzhichao@hust.edu.cn>
2021-11-01 20:37:10 +0800

    缓存路由组件对象

--- src/pages/Shop/Shop.vue
+++ src/pages/Shop/Shop.vue
@@ -12,7 +12,9 @@
         <router-link to="/shop/info" replace>商家</router-link>
       </div>
     </div>
-    <router-view/>
+    <keep-alive>
+      <router-view/>
+    </keep-alive>
   </div>
 </template>
 
