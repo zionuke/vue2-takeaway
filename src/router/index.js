785f92e6bbadf8521f29529af50a9fab10563d27
dragon-liu <liuzhichao@hust.edu.cn>
2021-11-01 21:31:29 +0800

    路由懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,21 @@
 */
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import MSite from '@/pages/MSite/MSite'
-import Search from '@/pages/Search/Search'
-import Order from '@/pages/Order/Order'
-import Profile from '@/pages/Profile/Profile'
-import Login from '@/pages/Login/Login'
-import Shop from '@/pages/Shop/Shop'
+
+// import MSite from '@/pages/MSite/MSite'
+// import Search from '@/pages/Search/Search'
+// import Order from '@/pages/Order/Order'
+// import Profile from '@/pages/Profile/Profile'
+// import Login from '@/pages/Login/Login'
+// import Shop from '@/pages/Shop/Shop'
+
+const MSite = () => import('../pages/MSite/MSite.vue')
+const Search = () => import('../pages/Search/Search.vue')
+const Order = () => import('../pages/Order/Order.vue')
+const Profile = () => import('../pages/Profile/Profile.vue')
+const Login = () => import('@/pages/Login/Login')
+const Shop = () => import('@/pages/Shop/Shop')
+
 import ShopGoods from '@/pages/Shop/ShopGoods/ShopGoods'
 import ShopRatings from '@/pages/Shop/ShopRatings/ShopRatings'
 import ShopInfo from '@/pages/Shop/ShopInfo/ShopInfo'
