4f71a641a87a292dfadfac590f977c0743a4f881
dragon-liu <liuzhichao@hust.edu.cn>
2021-11-01 22:41:33 +0800

    使用 date-fns 代替 moment

--- src/filters/index.js
+++ src/filters/index.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
-import moment from 'moment'
-// import format from 'date-fns/format'
+// import moment from 'moment'
+import format from 'date-fns/format'
 
 // 自定义过滤器
-Vue.filter('date-format', function (value, formatStr='YYYY-MM-DD HH:mm:ss') {
+Vue.filter('date-format', function (value, formatStr='yyyy-MM-dd HH:mm:ss') {
   
-  return moment(value).format(formatStr)
-  // return format(value, formatStr)
+  // return moment(value).format(formatStr)
+  return format(value, formatStr)
 })
